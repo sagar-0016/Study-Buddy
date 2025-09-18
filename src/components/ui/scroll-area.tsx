7d875159b110aff50d35c2a8491f3901dc634f70
Saurabh Sagar <ssnl1216sagarsaurabh@gmail.com>
2025-09-18 15:46:19 +0000

    makeit white in dark mode and make it black in light mode, not completel

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -41,7 +41,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-primary/50 hover:bg-primary" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-neutral-300 dark:bg-neutral-700" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
